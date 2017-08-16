c950fc2340a1b07ab0d26b7a68b1fa10af814496
Nis Wilson Nissen <niswilsonnissen@gmail.com>
2017-08-16 15:52:49 +0200

    refactor: Created ListsBooks component
    Extracted ListBooks component that holds three
    book shelves and is in charge of handling the list
    page.

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react'
 import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
-import Book from './Book.js'
-import BookShelf from './BookShelf.js'
+import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
 
@@ -307,31 +306,8 @@ class BooksApp extends React.Component {
           </div>
         )} />
         <Route exact path="/" render={( { history } ) => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <div className="list-books-content">
-                <div>
-                  <BookShelf
-                    title="Currently Reading"
-                    books={books.filter(b => b.shelf === "currentlyReading")}
-                  />
-                  <BookShelf
-                    title="Want to Read"
-                    books={books.filter(b => b.shelf === "wantToRead")}
-                  />
-                  <BookShelf
-                    title="Read"
-                    books={books.filter(b => b.shelf === "read")}
-                  />
-                </div>
-              </div>
-              <div className="open-search">
-                <Link to="/search">Add a book</Link>
-              </div>
-            </div>
-          )} />
+            <ListBooks books={books} />
+        )} />
       </div>
     )
   }
